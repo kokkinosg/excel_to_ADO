d23a42abce4d4ecc8e35f6101c1541c3e57b52ab
Georgios Kokkinos <gk769@bath.ac.uk>
2025-07-10 11:20:51 +0100

    Refactor DataSender class to improve variable encapsulation and fix instantiation of AzDClientApi

--- excel_to_ado/src/main/java/com/example/excel_to_ADO/DataSender.java
+++ excel_to_ado/src/main/java/com/example/excel_to_ADO/DataSender.java
@@ -1,21 +1,20 @@
 package com.example.excel_to_ADO;
-import java.io.IOException;
-
-import org.AzDClientApi;
+import org.azd.utils.AzDClientApi;
 
 
 public class DataSender {
     
     //#region Private variables
 
-    // URL for the Alpaca API
-    String organizationName = "SphereFludics";
-    String projectName = "Cyto-Mine";
-
+    // Private variables for the conenction object
+    private final String organizationName = "SphereFludics";
+    private final String projectName = "Cyto-Mine";
     // Personal access token obtained from User Settings on ADO - valid for 30 days (obtained: 10/07/2025)
+    // Note: It only has permissions to read and write workitems. 
     private static final String personalAccessToken = "G4brTLIy1dgtD8J2yBVrdH6ldZOKuD4Wst0xiuelDEyQFRTyxsRHJQQJ99BGACAAAAArd3bnAAASAZDOrJ91";
     
-
+    // Connection object
+    private AzDClientApi webApi;
     //#endregion
 
     //#region Constructor
@@ -23,11 +22,11 @@ public class DataSender {
     // Constructor 
 
     public DataSender() {
-
-        // Obtain an instance of the ADO api 
-        var webApi = new AzDClientApi(organizationName, projectName, personalAccessToken);
-
+        // Obtain an instance of the ADO api only when this class is instantiated
+        this.webApi = new AzDClientApi(organizationName, projectName, personalAccessToken);
     }
     //#endregion
 
     //#region Public methods
+
+}
