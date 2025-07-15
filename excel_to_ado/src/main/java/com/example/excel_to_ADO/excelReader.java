6b2ab5ef751c949296ccb90ae2e753b6e3402747
Georgios Kokkinos <gk769@bath.ac.uk>
2025-07-15 19:03:10 +0100

    Refactor excelReader class to streamline workbook and sheet opening process, enhancing constructor and method logic.

--- excel_to_ado/src/main/java/com/example/excel_to_ADO/excelReader.java
+++ excel_to_ado/src/main/java/com/example/excel_to_ADO/excelReader.java
@@ -1,10 +1,10 @@
 package com.example.excel_to_ADO;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -14,24 +14,28 @@ public class excelReader {
     // Private variables 
     private String filePath; // path of the excel file
     private Workbook workbook; // Workbook object
-    private FileInputStream file; //File object
-
+    private Sheet sheet;
+    
     
     // Constructor. 
-    // Takes the path to the excel file as an argument 
-    public excelReader(String filePath){
-        this.filePath = filePath;
+    // Takes the path to the excel file as an argument and the sheet number with the data and will open the xlsx, workbook and then the sheet. 
+    public excelReader(String filePath, String sheetName){
+
+
+        if (openXlsx(filePath) && openSheet(sheetName)){
+            // Code to execute when hte sheet is obtianed
+        }
     }
 
-
+    
 
 
     // Helper functions
 
-    // Helper method which opens the .xlsx file 
-    private  boolean openXlsx(){
-        try {
-            this.file = new FileInputStream(new File(filePath));
+    // Helper method which opens the workbook and leaves only the Workbook open.
+    // The FileInputStream is closed immediately after loading.
+    private boolean openXlsx(String filePath){
+        try (FileInputStream file = new FileInputStream(filePath)) { // This way the stream auto closes. 
             this.workbook = new XSSFWorkbook(file); 
             return true;
         } catch (FileNotFoundException e) { //Catch the file not found exception which can be thrown at FileInputStream
@@ -41,10 +45,18 @@ public class excelReader {
             System.out.println("IO Exception: " + e.getMessage());
             return false;
         }
-       
     }
 
+    // Method to open a sheet from a specified workbook. 
+    private boolean openSheet(String sheetName){
+        try {
+            this.sheet = workbook.getSheet(sheetName);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error when opening the sheet: " + e.getMessage());
+            return false;
+        }
+    }
 
-
-
+    
 }
