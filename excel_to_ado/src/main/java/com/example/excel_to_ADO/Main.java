480e48aa9942817e147afa56a24d9db1bfe2d4fe
Georgios Kokkinos <gk769@bath.ac.uk>
2025-07-15 10:27:33 +0100

    Refactor main method in Main class to ensure work item creation occurs only after successful connection test

--- excel_to_ado/src/main/java/com/example/excel_to_ADO/Main.java
+++ excel_to_ado/src/main/java/com/example/excel_to_ADO/Main.java
@@ -8,19 +8,20 @@ public class Main {
         adoRESTClient client = new adoRESTClient();
 
         // Check whether the connection is succesfull 
-        client.testConnection();
+        if(client.testConnection()){
+            // Test the work item ID finder
+            // String workItemTitle = "It shall be possible to validate Bootstrap camera images for image exposure";
+            // System.out.printf("ID: %d - Title: %s",client.getWorkItemID(workItemTitle),workItemTitle);
 
-        // Test the work item ID finder
-        // String workItemTitle = "It shall be possible to validate Bootstrap camera images for image exposure";
-        // System.out.printf("ID: %d - Title: %s",client.getWorkItemID(workItemTitle),workItemTitle);
+            // Create a sample work item
+            client.createWorkItem("User-Story", "TEST_PARENT_GK_1407025", "sample acceptance criteria", null, null);
+            // Get its ID
+            Integer id = client.getWorkItemID("TEST_PARENT_GK_1407025");
+            // Create a child of the sample work item using Hierarchy-Reverse
+            client.createWorkItem("User-Story", "TEST_CHILD_GK_1407025", "sample acceptance criteria", id, "Hierarchy-Reverse");
+            
+        }
 
-        // Create a sample work item
-        client.createWorkItem("User-Story", "TEST_PARENT_GK_1407025", "sample acceptance criteria", null, null);
-        // Get its ID
-        Integer id = client.getWorkItemID("TEST_PARENT_GK_1407025");
-        // Create a child of the sample work item using Hierarchy-Reverse
-        client.createWorkItem("User-Story", "TEST_CHILD_GK_1407025", "sample acceptance criteria", id, "Hierarchy-Reverse");
-        
     }
     
 }
